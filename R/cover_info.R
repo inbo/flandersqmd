78cc33653710a188cc3840b152a04fec8a41bda3
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-03-19 17:17:02 +0100

    ✨ Create a cover.txt when running post_render()

---
+++ R/cover_info.R
@@ -0,0 +1,31 @@
+cover_info <- function(yml) {
+  yml$title <- paste0(yml$title, "\nSubtitel: ", yml$subtitle)
+  yml$author$email[
+    !is.na(yml$author$corresponding) & yml$author$corresponding
+  ] |>
+    paste(collapse = ", ") -> yml$corresponding
+  apply(yml$author$name, 1, paste, collapse = ", ") |>
+    paste(collapse = ", ") -> yml$author
+  yml$print <- format_print_order(yml)
+  cover_txt <- sprintf(
+    "Titel: %s\nAuteur(s): %s\nContactpersoon: %s\nAfbeelding voor cover: %s
+Embargo tot: %s\n%s",
+    yml$title,
+    yml$author,
+    yml$corresponding,
+    paste0(yml$coverphoto, ""),
+    paste0(yml$embargo, ""),
+    yml$print
+  )
+  if (has_name(yml, "client_logo")) {
+    cover_txt <- c(cover_txt, paste("Logo klant:", yml$client_logo))
+  }
+  if (has_name(yml, "cooperation_logo")) {
+    cover_txt <- c(
+      cover_txt,
+      paste("Logo samenwerking:", yml$cooperation_logo)
+    )
+  }
+  writeLines(cover_txt, "cover.txt")
+  return(invisible(NULL))
+}
